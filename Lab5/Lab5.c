1304258c79de92a0b8f3e136be0b59b2cb32f30f
Louis-He <1726110778@qq.com>
2018-02-04 16:31:44 -0500

    Update Lab5

--- Lab5/Lab5.c
+++ Lab5/Lab5.c
@@ -5,10 +5,22 @@ int factorial (int n);
 
 
 int main(int argc, char **argv){
-	int rows;
+	int rows; int row = 0; int space = 0;
 	printf("Enter the number of rows: \n");
 	scanf("%d", rows);
-	
+
+	for(row = 0;row <= rows; row++){
+		// output space before the first characters 
+		for(space = 0; space <= rows - row - 2; space ++){
+			printf("   ");
+		}
+		
+		// output number
+
+
+
+	}
+
 	return 0;
 }
 
